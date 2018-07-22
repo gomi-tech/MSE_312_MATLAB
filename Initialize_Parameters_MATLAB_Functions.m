03cee5c3357afe0f92dcedfce98f70ed94fd3ea3
Ascellia <37527968+Ascellia@users.noreply.github.com>
2018-07-21 22:39:41 -0700

    started on PI controller
    May need to change the systems model up review of system equations with Peter tonight.

--- Initialize_Parameters_MATLAB_Functions.m
+++ Initialize_Parameters_MATLAB_Functions.m
@@ -15,7 +15,7 @@ K_d = 1.1e-4;       % Back EMF constant
 nu = 1/6.3;         %
 
 
-% Simplifying the A matrix here.
+% Simplifying the A matrix (no load) here.
 a = R/L;
 b = K_e*nu/L;
 c = V/L;
@@ -34,10 +34,12 @@ B = [c
      0];
 
 %% Make a separate A matrix for Load analysis
-J_L = 6319858.29e-9; % Moment of Inertia of Load/Truss 
+J_L = 1319858.29e-9; % Moment of Inertia of Load/Truss
+d_l = (K_t*nu)/( (J_L/E) + J_m*nu^2 );
+e_l = (K_d*nu^2)/( (J_L/E) + J_m*nu^2 );
 A_L = [-a 0 -b;
      0 0 1;
-     d 0 -e];
+     d_l 0 -e_l];
  
 %Form of the state space vector is [current ,position, velocity]
 %Therefore to analyze position and velocity response of the system
@@ -68,37 +70,166 @@ fvtool(b,a,'polezero');
 [b,a] = eqtflength(b,a);
 [z,p,k] = tf2zp(b,a);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%    GAIN ANANLYSIS - Position     %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%          GAIN ANANLYSIS                 %
+%*****************************************%
+%*********     (P)  **********************%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5%%%
+%------------------------------------------
+%    GAIN ANANLYSIS - Position no Load    %
+%------------------------------------------
 G_pos = sys_pos;
-k1 = 0.5;
+k1 = 0.1;
 k2 = 1;
-k3 = 0.1;
+k3 = 0.02;
 T1 = feedback(G_pos*k1,1);
 T2 = feedback(G_pos*k2,1);
 T3 = feedback(G_pos*k3,1);
-figure(2);
-step(T1,'b',T2,'r',T3,'g');
-legend('k = 0.5','k = 1','k = 0.1');
-title('Position-Gain Analysis');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%    GAIN ANANLYSIS - Velocity     %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure(2);
+subplot(2,1,1);
+step(T1,'b',T2,'r',T3,'g');
+legend('k = 0.1','k = 1','k = 0.02');
+title('Position-Gain Analysis no load');
+hold on
+
+
+%------------------------------------------
+%    GAIN ANANLYSIS - Position w/ Load    %
+%------------------------------------------
+G_pos_L = sys_pos_L;
+k1 = 0.1;
+k2 = 1;
+k3 = 0.02;
+T1 = feedback(G_pos_L*k1,1);
+T2 = feedback(G_pos_L*k2,1);
+T3 = feedback(G_pos_L*k3,1);
+
+subplot(2,1,2); 
+step(T1,'b',T2,'r',T3,'g');
+legend('k = 0.1','k = 1','k = 0.02');
+title('Position-Gain Analysis w/ load');
+hold off
+
+%-----------------------------------------
+%    GAIN ANANLYSIS - Velocity no Load    %
+%-----------------------------------------
 
 G_vel = sys_vel;
-k1 = 0.5;
+k1 = 0.1;
 k2 = 1;
-k3 = 2;
+k3 = 0.02;
 T1 = feedback(G_vel*k1,1);
 T2 = feedback(G_vel*k2,1);
 T3 = feedback(G_vel*k3,1);
 
 figure(3);
+subplot(2,1,1);
 step(T1,'b',T2,'r',T3,'g');
-legend('k = 0.5','k = 1','k = 2');
-title('Velocity-Gain Analysis');
+legend('k = 0.1','k = 1','k = 0.02');
+title('Velocity-Gain Analysis no load');
+hold on
+
+%-----------------------------------------
+%    GAIN ANANLYSIS - Velocity w/ Load    %
+%-----------------------------------------
+
+G_vel_L = sys_vel_L;
+k1 = 0.1;
+k2 = 1;
+k3 = 0.02;
+T1 = feedback(G_vel_L*k1,1);
+T2 = feedback(G_vel_L*k2,1);
+T3 = feedback(G_vel_L*k3,1);
 
 
+subplot(2,1,2); 
+step(T1,'b',T2,'r',T3,'g');
+legend('k = 0.1','k = 1','k = 0.02');
+title('Velocity-Gain Analysis w/ load');
+hold off
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         GAIN/INTEGRAL ANALYSIS          %
+%*****************************************%
+%******         PI      ******************%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5%%%
+%---------------------------------------------------
+%    GAIN/INTEGRAL ANANLYSIS - Position no Load    %
+%---------------------------------------------------
+
+G_pos = sys_pos;
+k2 = 0.1;
+ki1 = 10;
+ki2 = 1;
+ki3 = 0.1;
+T1 = feedback(G_pos*(k2+(ki1/s)),1);
+T2 = feedback(G_pos*(k2+(ki2/s)),1);
+T3 = feedback(G_pos*(k2+(ki3/s)),1);
+
+figure(4);
+subplot(2,1,1);
+step(T1,'b',T2,'r',T3,'g');
+legend('ki = 10','ki = 1','ki = 0.1');
+title('Position-PI Analysis no load');
+hold on
+
+
+%------------------------------------------
+%    GAIN ANANLYSIS - Position w/ Load    %
+%------------------------------------------
+G_pos_L = sys_pos_L;
+k2 = 0.1;
+ki1 = 10;
+ki2 = 1;
+ki3 = 0.1;
+T1 = feedback(G_pos_L*(k2+(ki1/s)),1);
+T2 = feedback(G_pos_L*(k2+(ki2/s)),1);
+T3 = feedback(G_pos_L*(k2+(ki3/s)),1);
+
+subplot(2,1,2); 
+step(T1,'b',T2,'r',T3,'g');
+legend('k = 0.5','k = 1','k = 0.1');
+title('Position-PI Analysis w/ load');
+hold off
+
+% %-----------------------------------------
+% %    GAIN ANANLYSIS - Velocity no Load    %
+% %-----------------------------------------
+% 
+% G_vel = sys_vel;
+% k1 = 0.5;
+% k2 = 1;
+% k3 = 2;
+% T1 = feedback(G_vel*k1,1);
+% T2 = feedback(G_vel*k2,1);
+% T3 = feedback(G_vel*k3,1);
+% 
+% figure(3);
+% subplot(2,1,1);
+% step(T1,'b',T2,'r',T3,'g');
+% legend('k = 0.5','k = 1','k = 2');
+% title('Velocity-Gain Analysis no load');
+% hold on
+% 
+% %-----------------------------------------
+% %    GAIN ANANLYSIS - Velocity w/ Load    %
+% %-----------------------------------------
+% 
+% G_vel_L = sys_vel_L;
+% k1 = 0.5;
+% k2 = 1;
+% k3 = 2;
+% T1 = feedback(G_vel_L*k1,1);
+% T2 = feedback(G_vel_L*k2,1);
+% T3 = feedback(G_vel_L*k3,1);
+% 
+% 
+% subplot(2,1,2); 
+% step(T1,'b',T2,'r',T3,'g');
+% legend('k = 0.5','k = 1','k = 2');
+% title('Velocity-Gain Analysis w/ load');
+% hold off
+% 
+% 
